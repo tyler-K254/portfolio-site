ec86550c55a143b659e8bcfbd28bfbe9f1905927
tyler kirumba <tylerkirumba@gmail.com>
2023-04-16 17:34:57 +0300

    updated

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ setTimeout(function fade(){
     $('.banner').fadeOut();
 },2000);
 
-fade();
+
+
+$('.sample_image').click(function(){
+    console.log('working') 
+});
+
+// let image = document.querySelector('.sample_image');
+// image.addEventListener('click', function(){
+//     console.log('working')
+// })
 
